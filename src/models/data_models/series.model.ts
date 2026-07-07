69dc2ef9c1ec6528460e5097b29e2d752047c6f0
brau25-bit <brauliochaveza@gmail.com>
2026-07-06 22:09:08 -0600

    add: modelo de datos para mensaje de series

---
+++ src/models/data_models/series.model.ts
@@ -0,0 +1,11 @@
+
+type SeriesDiscoverMessage = {
+    id: string,
+    source: string,
+    title: string,
+    cover: string,
+    status: string,
+    url: string
+}
+
+export default SeriesDiscoverMessage
