d7244a0df48cc243bad99d5036eb61ccea2a00ab
brau25-bit <brauliochaveza@gmail.com>
2026-07-06 22:01:27 -0600

    fix: manejo de errores, DLQ para fallos dentro de la cola

--- src/rabbitMQ/connection.ts
+++ src/rabbitMQ/connection.ts
@@ -1,24 +1,26 @@
 import amqp, {Channel, ChannelModel} from 'amqplib'
 import 'dotenv/config'
+import { config } from '../config/config.js'
 
 export class RabbitClient{
     static async createConnection(): Promise<ChannelModel>{
         try {
-            const RABBIT_URL = process.env.RABBITMQ_URL
+            const RABBIT_URL = config.rabbitMQ
 
             const connection: ChannelModel = await amqp.connect(RABBIT_URL!)
 
-            connection.on('error', () => {
-                throw new Error("Error on connection")
+            connection.on('error', (err) => {
+                throw err
             })
 
-            connection.on('close', () => {
-                throw new Error("Connection was closed")
+            connection.on('close', (err) => {
+                throw err
             })
 
             return connection
         } catch (error) {
             console.log("Error on connection", error)
+
             await new Promise(res => setTimeout(res, 5000))
 
             return this.createConnection()
@@ -26,9 +28,13 @@ export class RabbitClient{
     }
 
     static async createChannel(connection: ChannelModel): Promise<Channel> {
-        const channel: Channel = await connection.createChannel()
+        try {
+            const channel: Channel = await connection.createChannel()
 
-        return channel
+            return channel
+        } catch (error) {
+            throw error
+        }
     }
 
     static setPrefetch(channel: Channel, prefetch: number) {
@@ -36,21 +42,34 @@ export class RabbitClient{
     }
 
     static async declareQueue(channel: Channel, queue: string){
-        const q = await channel.assertQueue(
-            queue,
-            {
-                durable: true,
-                arguments: {
-                    'x-queue-type': 'classic'
+        try {
+            await channel.assertQueue(
+                queue,
+                {
+                    durable: true,
+                    arguments: {
+                        'x-queue-type': 'classic',
+                        'x-dead-letter-exchange': "",
+                        'x-dead-letter-routing-key': `${queue}.dlq`
+                    }
                 }
-            }
-        )
+            );
 
-        return q
+            await channel.assertQueue(`${queue}.dlq`, {
+                durable: true
+            });
+
+        } catch (error) {
+            throw new Error("Invalid Queue")
+        }
     }
 
     static async closeConnection(connection: ChannelModel, channel: Channel) {
-        await channel.close()
-        await connection.close()
+        try {
+            await channel.close()
+            await connection.close()
+        } catch (error) {
+            throw error
+        }
     }
 }
