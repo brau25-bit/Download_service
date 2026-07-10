8660cae3563ddbec2fd110b088245ce5664169dd
brau25-bit <brauliochaveza@gmail.com>
2026-07-09 20:22:05 -0600

    add: modelo de respuesta para postgre y servicio

---
+++ src/models/response_models/series.res.ts
@@ -0,0 +1,12 @@
+
+type SeriesResponse = {
+    id:     string,
+    source: string,
+    title:  string,
+    cover:  string,
+    status: string,
+    url:    string,
+    dir:    string
+}
+
+export default SeriesResponse
