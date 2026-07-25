8b97f3652c86815d4243c396e5388edab6a54800
brau25-bit <brauliochaveza@gmail.com>
2026-07-24 21:56:01 -0600

    add: DTO para capitulos

---
+++ src/models/response_models/chapter.res.ts
@@ -0,0 +1,12 @@
+
+type ChapterDTO = {
+    id?: string,
+    serie_id: string,
+    chapter_url: string,
+    chapter_number: number,
+    system_path: string
+    created?: string,
+    updated_at?: string 
+}
+
+export default ChapterDTO
