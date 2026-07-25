00e53694df46f841188ebd1e6865314106af92ef
brau25-bit <brauliochaveza@gmail.com>
2026-07-24 21:56:35 -0600

    add: DTO para imagenes de capitulos

---
+++ src/models/response_models/images.res.ts
@@ -0,0 +1,12 @@
+
+type ImagesDTO = {
+    id?: string,
+    chapter_id: string,
+    img_url: string,
+    img_number: number,
+    system_path: string
+    created?: string,
+    updated_at?: string
+}
+
+export default ImagesDTO
