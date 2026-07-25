612cf4cd6bd203cfcc94708f2cfef7428edf3b38
brau25-bit <brauliochaveza@gmail.com>
2026-07-24 21:52:46 -0600

    update: actualizacion de campos faltantes dentro de DTO

--- src/config/pg.config.ts
+++ src/config/pg.config.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 
 export type Config = {
+    host?: string,
     user?: string,
     password?: string,
     port?: number,
+    max?: number,
     database?: string,
     statement_timeout?: number,
     query_timeout?: number,
