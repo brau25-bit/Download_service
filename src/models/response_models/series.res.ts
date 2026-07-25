8d1ba1c72d192ae8e750c0e2670b750d3f4aa94b
brau25-bit <brauliochaveza@gmail.com>
2026-07-24 21:55:16 -0600

    fix: añadido de los campos faltantes

--- src/models/response_models/series.res.ts
+++ src/models/response_models/series.res.ts
@@ -5,8 +5,10 @@ type SeriesResponse = {
     title:  string,
     cover:  string,
     status: string,
-    url:    string,
-    dir:    string
+    serie_url:    string,
+    system_path:    string,
+    created?: string,
+    updated_at?: string
 }
 
 export default SeriesResponse
