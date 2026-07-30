f02f81d4a9c78bbb59a9b8c540baaca446c5c2e5
brau25-bit <brauliochaveza@gmail.com>
2026-07-29 20:04:56 -0600

    update: actualizacion de parametros y añadido de metodo para busqueda por titulo

--- src/repository/manga_repository.ts
+++ src/repository/manga_repository.ts
@@ -2,15 +2,46 @@ import { ConnectionPg } from "../database/database_connection.js";
 import SeriesResponse from "../models/response_models/series.res.js";
 import ChapterDTO from "../models/response_models/chapter.res.js";
 import ImagesDTO from "../models/response_models/images.res.js";
+import { Chapter } from "../models/data_models/chapter.models.js";
 
-class PgClient {
+export class MangaRepository {
 
-    static async getSeries(){
-        
+    static async getSeries(params: Chapter){
+        const query: string = `
+            SELECT * FROM series
+        `;
     }
 
-    static async getSerie(){
-    
+    static async getSerieById(id: string): Promise<SeriesResponse>{
+        const query_id: string = `
+            SELECT * FROM series
+            WHERE (id) = $1
+        `;
+
+        const data: string[] = [id];
+
+        const result = await ConnectionPg.query<SeriesResponse>(query_id, data);
+
+        if(result.rowCount == 0) throw new Error("No data found");
+
+        return result.rows[0]!;
+    }
+
+    static async getSerieByTitle(title: string): Promise<SeriesResponse>{
+        if(!title) throw new Error("Title was expected, but was not received");
+
+        const query: string = `
+            SELECT * FROM series
+            WHERE(title) = $1
+        `;
+
+        const data: string[] = [title];
+
+        const result = await ConnectionPg.query<SeriesResponse>(query, data);
+
+        if(!result) throw new Error("not found");
+
+        return result.rows[0]!;
     }
 
     static async getChapter(){
@@ -23,8 +54,9 @@ class PgClient {
 
         const query: string = `
             INSERT INTO 
-            series(id,source,title,cover,status,url,dir)
+            series(id,source,title,cover,serie_url,system_path)
             VALUES($1, $2, $3, $4, $5, $6)
+            ON CONFLICT(id) DO NOTHING
             RETURNING *;
         `;
 
@@ -46,7 +78,8 @@ class PgClient {
         return result.rows[0];
     }
 
-    static async saveChapter(params: ChapterDTO){
+    static async saveChapter(params: ChapterDTO): Promise<ChapterDTO>{
+
         const {serie_id, chapter_url, chapter_number, system_path} = params;
         let result;
 
@@ -54,6 +87,7 @@ class PgClient {
             INSERT INTO 
             chapters(serie_id, chapter_url, chapter_number, system_path) 
             VALUES($1, $2, $3, $4)
+            RETURNING *
         `;
 
         const data = [
@@ -66,13 +100,15 @@ class PgClient {
         try {
             result = await ConnectionPg.query<ChapterDTO>(query, data);
         } catch (error) {
-            throw error;   
+            throw error;
         }
 
-        return result.rows[0];
+        if(result.rowCount == 0) throw new Error("not found");
+
+        return result.rows[0]!;
     }
 
-    static async saveChapterImages(params: ImagesDTO){
+    static async saveChapterImages(params: ImagesDTO): Promise<ImagesDTO>{
         const {chapter_id, img_url, img_number, system_path} = params;
         let result;
 
@@ -80,6 +116,7 @@ class PgClient {
             INSERT INTO 
             images(chapter_id, img_url, img_number, system_path)
             VALUES($1,$2,$3,$4)
+            RETURNING *
         `;
 
         const data = [
@@ -95,6 +132,6 @@ class PgClient {
             throw error;
         }
 
-        return result.rows[0];
+        return result.rows[0]!;
     }
 } 
