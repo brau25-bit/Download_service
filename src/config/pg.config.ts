8b2e930b863bd0d7b8492a73cffbe2aad9d5fb09
brau25-bit <brauliochaveza@gmail.com>
2026-07-30 21:47:52 -0600

    update: actualizacion de configuraciones

--- src/config/pg.config.ts
+++ src/config/pg.config.ts
@@ -1,10 +1,4 @@
-import 'dotenv/config'
-
 export type Config = {
-    host?: string,
-    user?: string,
-    password?: string,
-    port?: number,
     max?: number,
     database?: string,
     statement_timeout?: number,
