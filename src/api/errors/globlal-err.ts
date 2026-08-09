a930c1cdc25a4022e8321c40697fb1b0c2c22e61
brau25-bit <brauliochaveza@gmail.com>
2026-08-09 13:14:34 -0600

    update: manejo de error global

--- src/api/errors/globlal-err.ts
+++ src/api/errors/globlal-err.ts
@@ -1,7 +1,7 @@
 
 export class ErrorHandler extends Error {
     constructor(message: string, public statusCode: number){
-        //if(!message) message = "internal server error";
+        if(!message) message = "internal server error";
         
         super(message);
         
