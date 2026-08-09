10aab27456d79e8e862129ef2dfd11d5bc4b1102
brau25-bit <brauliochaveza@gmail.com>
2026-08-09 13:16:09 -0600

    update: servicio de archivos estaticos

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,14 +1,22 @@
 import manga_router from './routes/manga.routes.js';
 import chapter_router from './routes/chapter.routes.js';
 import { errorHandler } from './middleware/globlal-err-handler.js';
+import { config } from '../config/config.js';
 
 import 'dotenv/config'
 import Express from 'express';
+import health_router from './routes/health.js';
+import cors from 'cors';
 
 const server = Express();
 
+server.use(cors());
+
 server.use('/manga', manga_router);
 server.use('/chapter', chapter_router);
+server.use('/health', health_router)
+
+server.use('/images', Express.static(config.series_path))
 
 server.use(errorHandler);
 
